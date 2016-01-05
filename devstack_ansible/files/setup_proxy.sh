4e7b539781e71a861432bcb31a23a545e311528a
Nate Potter <nathaniel.potter@intel.com>
2016-01-05 23:40:16 +0000

    Add proxy setup script

---
+++ devstack_ansible/files/setup_proxy.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Script to set up Intel proxy environment variables
+#
+# Nate Potter
+
+exit_with_error () {
+    echo "$1" 1>&2
+    exit 1
+}
+
+if [[ $EUID -ne 0 ]];then
+    exit_with_error "Please run this script as root"
+fi   
+
+# Update .bashrc with proxy variables
+
+echo 'export ftp_proxy=http://proxy-us.intel.com:911
+export http_proxy=http://proxy-us.intel.com:911
+export https_proxy=https://proxy-us.intel.com:911
+export no_proxy=intel.com,.intel.com,10.0.0.0/8,192.168.0.0/16,localhost,127.0.0.0/8,134.134.0.0/16,172.16.0.0/20
+export GIT_PROXY_COMMAND=~/bin/socks-gw
+export PATH=~/bin:$PATH' >> ~/.bashrc || exit_with_error "Failed to update .bashrc!"
+
+echo "Updated .bashrc..."
+
+# Create proxy scripts in ~/bin
+
+mkdir ~/bin && \
+echo '#!/bin/sh
+$1 = hostname, $2 = port
+proxy=proxy-socks.sc.intel.com
+exec socat STDIO SOCKS4:$proxy:$1:$2' > ~/bin/git-proxy && \
+echo '#!/bin/sh
+MODE="GNOME"
+echo $1 | grep "\.intel\.com$" > /dev/null 2>&1
+if [ $? -eq 0 ];then
+    connect $@
+else
+    connect -S proxy-socks.sc.intel.com:1080 $@
+fi' > ~/bin/socks-gw && \
+chmod a+x ~/bin -R || exit_with_error "Failed to create proxy scripts!"
+
+echo "Created proxy scripts..."
+
+# Update environment variables
+
+echo 'http_proxy="http://proxy-us.intel.com:911"
+https_proxy="https://proxy-us.intel.com:911"
+ftp_proxy="http://proxy-us.intel.com:911"
+no_proxy="intel.com,.intel.com,10.0.0.0/8,192.168.0.0/16,localhost,127.0.0.0/8,134.134.0.0/16,172,16.0.0/20"' >> /etc/environment || exit_with_error "Failed to update /etc/environment!"
+
+echo "Updated /etc/environment..."
+
+# Update sudoers file
+
+cp /etc/sudoers /etc/sudoers.tmp && \
+chmod 0640 /etc/sudoers.tmp && \
+echo 'Defaults  env_keep+="http_proxy https_proxy no_proxy HTTP_PROXY HTTPS_PROXY NO_PROXY"' >> /etc/sudoers.tmp && \
+chmod 0440 /etc/sudoers.tmp && \
+mv /etc/sudoers.tmp /etc/sudoers || exit_with_error "Failed to update sudoers!"
+
+echo "Updated sudoers..."
+
+echo "Proxy setup complete! Restart your terminal for it to take effect."
